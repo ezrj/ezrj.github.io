eb1c609564c154c536bbf3cd1ed9468d26dd14b5
ee <ezrajeter@gmail.com>
2024-08-11 22:45:15 -0500

    More tweaks

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -5,7 +5,7 @@ const navBar = {
 
 // Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  gradientColors: "#b08e75, #b99a83, #c3a897, #cebbaa, #decebe",
   firstName: "Ezra",
   middleName: "",
   lastName: "J.",
